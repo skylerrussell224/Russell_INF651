f4a76c7e5afad64cd63678e7107d48943dbbe2c9
skylerrussell224 <skylerrussell224@gmail.com>
2025-08-29 11:34:59 -0500

    Add Assignment 2 and Rearrange assignment 1

---
+++ week 2/assignment2/Challenges.js
@@ -0,0 +1,65 @@
+//Challenge 1
+let name = "John Doe";
+let age = 21;
+let isStudent = true;
+
+console.log(name)
+console.log(age)
+console.log(isStudent)
+
+//Challenge 2
+let num1 = 4;
+let num2 = 7;
+
+let addition = num1 + num2;
+let subtraction = num1 - num2;
+let multiplication = num1 * num2;
+let division = num1 / num2;
+
+console.log(addition);
+console.log(subtraction);
+console.log(multiplication);
+console.log(division);
+
+//Challenge 3
+const sentence = "This is a string";
+
+console.log(length.sentence)
+console.log(sentence.charAt(0))
+console.log(sentence.charAt(sentence.length-1))
+
+//Challenge 4
+let num3 = -9;
+
+console.log(Math.sqrt(num3))
+console.log(Math.pow(num3, 3))
+console.log(Math.abs(num3))
+
+//Challenge 5
+let num4 = 1;
+let num5 = 10;
+
+if (num4 > num5){
+    console.log(num4 + " is bigger")
+}
+if (num4 == num5){
+    console.log(num4 + " and " +num5 + " are equal")
+}
+if (num4 < num5){
+    console.log(num5 + " is bigger")
+}
+
+//Challenge 6
+let bool1 = true;
+let bool2 = false;
+
+console.log("bool1 && bool2: " + (bool1 && bool2))
+console.log("bool1 || bool2: " + (bool1 || bool2))
+console.log("bool1 != bool2: " + (bool1 != bool2))
+
+//Challenge 7
+let firstName = "John";
+let lastName = "Doe";
+let greeting = `Hello ${firstName} ${lastName}`
+
+console.log(greeting)
